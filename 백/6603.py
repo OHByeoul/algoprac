80012cc40e0f1525432f5318b3150d25603c8083
OHByeoul <suriessay@naver.com>
2024-09-29 23:39:50 +0900

    6603 다른방식으로 작성

--- 백/6603.py
+++ 백/6603.py
@@ -1,30 +1,47 @@
-
-
-def combi(index, level):
-	global choice, lst, n
-	#base
-	if level == 6:
-		for i in choice:
-			print(i, end=' ')
-		print()
-		return
-
-
-	for i in range(index, n):
-		choice.append(lst[i])
-		combi(i+1, level+1)
-		choice.pop()
-
+from itertools import combinations
 
 
 while True:
-	lst = list(map(int,input().split()))
-	n = lst[0]
-	lst = lst[1:]
-	choice = []
+	I = list(map(int,(input().split())))
 
-	if n == 0:
+	k = I[0]
+	arr = I[1:]
+
+	if k == 0:
 		break
 
-	combi(0,0)
+	for each in combinations(arr, 6):
+		for u in each:
+			print(u, end= ' ')
+		print()
 	print()
+
+
+# def combi(index, level):
+# 	global choice, lst, n
+# 	#base
+# 	if level == 6:
+# 		for i in choice:
+# 			print(i, end=' ')
+# 		print()
+# 		return
+
+
+# 	for i in range(index, n):
+# 		choice.append(lst[i])
+# 		combi(i+1, level+1)
+# 		choice.pop()
+
+
+
+# while True:
+# 	lst = list(map(int,input().split()))
+# 	n = lst[0]
+# 	lst = lst[1:]
+# 	choice = []
+
+# 	if n == 0:
+# 		break
+
+# 	combi(0,0)
+# 	print()
